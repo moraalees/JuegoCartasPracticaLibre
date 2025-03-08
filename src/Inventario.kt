ff8cb9c41ee8a3aa4965901f45a98a14c6be534c
cmorcan602 <mccristian@iesrafaelalberti.es>
2025-03-08 11:53:03 +0100

    clase genérico inventario hecha

---
+++ src/Inventario.kt
@@ -0,0 +1,41 @@
+package src
+
+class Inventario<T>(
+    private val elementos: MutableList<T> = mutableListOf()
+) {
+
+    private fun estaVacio(): Boolean {
+        return elementos.isEmpty()
+    }
+
+    fun agregarElemento(elemento: T) {
+        if (elemento in elementos){
+            println("El elemento ya está en el inventario.")
+        } else {
+            elementos.add(elemento)
+            println("Elemento agregado al inventario correcatmente.")
+        }
+    }
+
+    fun eliminarElemento(elemento: T) {
+        if (elementos.remove(elemento)) {
+            println("Elemento eliminado del inventario correctamente")
+        } else {
+            println("El elemento no está en el inventario")
+        }
+    }
+
+    fun mostrarElementos() {
+        if (estaVacio()) {
+            println("El inventario está vacío.")
+        } else {
+            println("Elementos en el inventario:")
+            elementos.forEach { elemento ->
+                var contador = 1
+                println("$contador. $elemento")
+                contador++
+            }
+        }
+    }
+
+}
